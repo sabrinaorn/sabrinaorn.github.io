8edd7ded0f31c52350f5e017a04798e8e5f44a2f
sabrinaorn <77697020+sabrinaorn@users.noreply.github.com>
2021-02-10 18:48:08 -0500

    Added new chapters, commented out Deacon Brodie's

--- story.js
+++ story.js
@@ -22,6 +22,94 @@ var config = {
         },
         onChapterEnter: [],
         onChapterExit: []
-      }
+      },
+       {
+        id: 'intro-identifier',
+        title: 'How the Virus has Affected Businesses',
+        image: 'C:/Users/sabri/Documents/Fourth Year Work/Internet GEOM 4001/Assignment One/Assignement1_Template/images/closed_business.jpg',
+        description: 'Mandatory closures of businesses including restaurants has meant that businesses may struggle to keep afloat during and after the pandemic. The following will highlight some of those stories.',
+        location: {
+          center: [-77.020636, 38.886900],
+          zoom: 13.5,
+          pitch: 60,
+          bearing: -43.2
+        },
+        onChapterEnter: [],
+        onChapterExit: []
+    },
+        {
+        id: 'slug-style-id',
+        title: 'Highlander Pub',
+        //image: './path/to/image/source.png',
+        description: 'A well known pub on Rideau St.',
+        location: {
+            center: [-75.69134, 45.42669],
+            zoom: 13.5,
+            pitch: 60,
+            bearing: 0
+        },
+        onChapterEnter: [
+            // {
+            //     layer: 'layer-name',
+            //     opacity: 1
+            // }
+        ],
+        onChapterExit: [
+            // {
+            //     layer: 'layer-name',
+            //     opacity: 0
+            // }
+        ]
+      },
+    {
+        id: 'other-identifier',
+        title: 'Wheelhouse Warehouse',
+        //image: './path/to/image/source.png',
+        description: 'Wheelhouse is a gym that is based around group indoor cycling class. As a response to the pandemic and provincial measures regarding the gyms and exercise class Wheelhouse had to continuously adapt its business model and locations. Prior to the pandemic Wheelhouse had a location on West Wellington and Bank street. Neither of these locations could adapt to the provincial requirements for gyms to open so they opened Wheelhouse at the Warehouse location and permanently closed their doors at the Wellington location.',
+        location: {
+            center: [-75.716660, 45.408750],
+            zoom: 13.5,
+            pitch: 60,
+            bearing: -43.2
+        },
+        onChapterEnter: [],
+        onChapterExit: []
+      },
+        {
+        id: 'other-identifier',
+        title: 'Plant and Curio',
+        //image: './path/to/image/source.png',
+        description: 'Plant and Curio though not permanently closed adapted its business model during the summer-fall months of the pandemic to have outdoors shopping on the patio/steps of their shop. During winter prior to the shutdown they switched models to primarily online to deal with reduced capacity allowed in store.',
+        location: {
+            center: [-75.712900, 45.406850,],
+            zoom: 13.5,
+            pitch: 60,
+            bearing: -43.2
+        },
+        onChapterEnter: [],
+        onChapterExit: []
+      },
+   /*     {
+          id: 'Temporary_Closures',
+          title: 'Bar Hopping on Elgin St Leads to Five Temporary Closures',
+          image: '.img/download.png',
+          description:'Elgin St. saw five temporary closures of bars and restaurants from two individuals who were awaiting their COVID test results. These individuals visted those locations while awaiting their tetst results and when the tests came back positive, the businesses voluntarily closed for two weeks to test their employees to ensure safe operations. It is unknown how much revenue was lost during these closures, however, it is important to note how badly that revenue was needed because of restricted operations. The affected businesses were:',
+
+        <ul>
+          <li> Deacon Brodies </li>
+          <li> St Louis Bar and Grill</li>
+          <li> Elgin Beer Project</li>
+          <li> Lieutenants Pump</li>
+          <li> Waller Street Brewing</li>
+          </ul>
+          location: {
+              center: [-75.68819, 45.41577],
+              zoom: 13.5,
+              pitch: 60,
+              bearing: -43.2
+          },
+          onChapterEnter: [],
+          onChapterExit: []
+        } */
       ]
 };
