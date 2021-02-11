900b1faceecf00f3111548004dc923102d5ac527
sabrinaorn <77697020+sabrinaorn@users.noreply.github.com>
2021-02-10 19:40:23 -0500

    adjusted zoom levels

--- story.js
+++ story.js
@@ -16,7 +16,7 @@ var config = {
         description: 'Mandatory closures of businesses including restaurants has meant that businesses may struggle to keep afloat during and after the pandemic. The following will highlight some of those stories.',
         location: {
           center: [-75.69134, 45.42669],
-          zoom: 0,
+          zoom: 12,
           pitch: 60,
           bearing: -43.2
         },
@@ -30,7 +30,7 @@ var config = {
         description: 'A well known pub on Rideau St.',
         location: {
             center: [-75.69134, 45.42669],
-            zoom: 13.5,
+            zoom: 16,
             pitch: 60,
             bearing: 0
         },
@@ -54,7 +54,7 @@ var config = {
         description: 'Wheelhouse is a gym that is based around group indoor cycling class. As a response to the pandemic and provincial measures regarding the gyms and exercise class Wheelhouse had to continuously adapt its business model and locations. Prior to the pandemic Wheelhouse had a location on West Wellington and Bank street. Neither of these locations could adapt to the provincial requirements for gyms to open so they opened Wheelhouse at the Warehouse location and permanently closed their doors at the Wellington location.',
         location: {
             center: [-75.716660, 45.408750],
-            zoom: 13.5,
+            zoom: 16,
             pitch: 60,
             bearing: -43.2
         },
@@ -68,7 +68,7 @@ var config = {
         description: 'Plant and Curio though not permanently closed adapted its business model during the summer-fall months of the pandemic to have outdoors shopping on the patio/steps of their shop. During winter prior to the shutdown they switched models to primarily online to deal with reduced capacity allowed in store.',
         location: {
             center: [-75.712900, 45.406850,],
-            zoom: 13.5,
+            zoom: 16,
             pitch: 60,
             bearing: -43.2
         },
@@ -90,7 +90,7 @@ var config = {
           </ul> */
           location: {
               center: [-75.68819, 45.41577],
-              zoom: 13.5,
+              zoom: 16,
               pitch: 60,
               bearing: -43.2
           },
