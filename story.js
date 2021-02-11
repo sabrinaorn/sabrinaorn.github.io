ad2b4ed116e11b639dda5c55abbd45f7b2215778
therev14 <78615324+therev14@users.noreply.github.com>
2021-02-10 21:27:14 -0500

    Update story.js

--- story.js
+++ story.js
@@ -79,15 +79,7 @@ var config = {
           id: 'Temporary_Closures',
           title: 'Bar Hopping on Elgin St Leads to Five Temporary Closures',
           image: '/images/collage.jpg',
-          description:'Elgin St. saw five temporary closures of bars and restaurants from two individuals who were awaiting their COVID test results. These individuals visted those locations while awaiting their tetst results and when the tests came back positive, the businesses voluntarily closed for two weeks to test their employees to ensure safe operations. It is unknown how much revenue was lost during these closures, however, it is important to note how badly that revenue was needed because of restricted operations. The affected businesses were:',
-
-     /*   <ul>
-          <li> Deacon Brodies </li>
-          <li> St Louis Bar and Grill</li>
-          <li> Elgin Beer Project</li>
-          <li> Lieutenants Pump</li>
-          <li> Waller Street Brewing</li>
-          </ul> */
+          description:'Elgin St. saw five temporary closures of bars and restaurants from two individuals who were awaiting their COVID test results. These individuals visted those locations while awaiting their tetst results and when the tests came back positive, the businesses voluntarily closed for two weeks to test their employees to ensure safe operations. It is unknown how much revenue was lost during these closures, however, it is important to note how badly that revenue was needed because of restricted operations. The affected businesses were: Deacon Brodies, Waller Street Brewing, Elgin Beer Project, St Louis Bar and Grill, and Lieutenants Pump.',
           location: {
               center: [-75.68819, 45.41577],
               zoom: 16,
@@ -96,6 +88,20 @@ var config = {
           },
           onChapterEnter: [],
           onChapterExit: []
-        } 
+        }
+         {
+          id: 'chart',
+          title: 'Tracking Positive Cases in Ottawa since February 2020',
+          image: '.img/oph.jpg',
+          description: '<canvas id="myChart" width="400" height="400"></canvas>',
+          location: {
+              center: [-75.67532, 45.40719],
+              zoom: 13.5,
+              pitch: 60,
+              bearing: -43.2
+          },
+          onChapterEnter: [],
+          onChapterExit: []
+}
       ]
 };
