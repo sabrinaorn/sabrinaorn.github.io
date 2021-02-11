fee7bfe11adb89e1a448946d7a8031682d61ecb0
Olive <71465779+Oliolipop11@users.noreply.github.com>
2021-02-11 18:15:30 -0500

    Update story.js

--- story.js
+++ story.js
@@ -51,7 +51,7 @@ var config = {
         id: 'olive1',
         title: 'Wheelhouse Warehouse',
         image: '/images/wheelhouse.jpg',
-        description: 'Wheelhouse is a gym that is based around group indoor cycling class. As a response to the pandemic and provincial measures regarding the gyms and exercise class Wheelhouse had to continuously adapt its business model and locations. Prior to the pandemic Wheelhouse had a location on West Wellington and Bank street. Neither of these locations could adapt to the provincial requirements for gyms to open so they opened Wheelhouse at the Warehouse location and permanently closed their doors at the Wellington location. <iframe width="560" height="315" src="https://www.youtube.com/embed/gbOuZlEk5cQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        description: 'Wheelhouse is a gym that is based around group indoor cycling class. As a response to the pandemic and provincial measures regarding the gyms and exercise class Wheelhouse had to continuously adapt its business model and locations. Prior to the pandemic Wheelhouse had a location on West Wellington and Bank street. Neither of these locations could adapt to the provincial requirements for gyms to open so they opened Wheelhouse at the Warehouse location and permanently closed their doors at the Wellington location. <br> <iframe width="560" height="315" src="https://www.youtube.com/embed/gbOuZlEk5cQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
         location: {
             center: [-75.716660, 45.408750],
             zoom: 16,
